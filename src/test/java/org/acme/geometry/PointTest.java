3153d144cb4e69c3c557dc3ca497d4a9611d0ed4
FlorentinBr <florentin.brisebard@ensg.eu>
2021-01-04 15:59:43 +0100

    Création classe point et lineString

---
+++ src/test/java/org/acme/geometry/PointTest.java
@@ -0,0 +1,17 @@
+package org.acme.geometry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PointTest {
+	
+	public static final double EPSILON = 1.0e-15;
+	
+	@Test
+	public void testType(){
+		// TODO
+		Point p = new Point();
+		Assert.assertEquals("Point", p.getType(), EPSILON);
+	}
+
+}

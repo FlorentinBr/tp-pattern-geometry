827666b975d2b97f964c89b9be72eb5f5ca286d1
FlorentinBr <florentin.brisebard@ensg.eu>
2021-01-05 12:01:04 +0100

    Ajout test 9.0

--- src/main/java/org/acme/geometry/LogGeometryVisitor.java
+++ src/main/java/org/acme/geometry/LogGeometryVisitor.java
@@ -4,7 +4,7 @@ public class LogGeometryVisitor implements GeometryVisitor {
 
 	@Override
 	public void visit(Point point) {
-		System.out.println("Je suis un point avec x=" + point.getCoordinate().getX() +  "et y=" + point.getCoordinate().getY());
+		System.out.println("Je suis un point avec x=" + point.getCoordinate().getX() +  " et y=" + point.getCoordinate().getY());
 	}
 
 	@Override
---
+++ src/test/java/org/acme/geometry/LogGeometryVisitorTest.java
@@ -0,0 +1,16 @@
+package org.acme.geometry;
+
+import org.junit.Test;
+
+public class LogGeometryVisitorTest {
+
+    @Test
+    public void testGeometryVisitor(){
+        LogGeometryVisitor visitor = new LogGeometryVisitor();
+        Geometry geometryPoint = SampleFactory.createPointA();
+        geometryPoint.accept(visitor);
+        Geometry geometryLine = SampleFactory.createLineStringOA();
+        geometryLine.accept(visitor);
+    }
+
+}
